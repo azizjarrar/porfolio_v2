9091244564c8d172a85b08e95e9652a0e96648da
azizjarrar <aziz_jarrar@hotmail.com>
2019-10-27 15:07:44 +0100

    gadit about me

--- portfolio/src/WelcomePage/WelcomePage.js
+++ portfolio/src/WelcomePage/WelcomePage.js
@@ -8,52 +8,67 @@ import contactme from './images/aboutme.png'
 class WelcomePage extends React.Component {
     constructor(props) {
         super(props)
+        this.state={
+            out:false
+        }
         this.gotoaboutme = this.gotoaboutme.bind(this)
         this.gotomyskills = this.gotomyskills.bind(this)
         this.gotocontactme = this.gotocontactme.bind(this)
     }
-
+    componentDidMount(){
+        this.setState({out:false})
+    
+    }
     gotoaboutme() {
-        this.props.history.push('/Aboutme')
+        this.setState({out:true})
+        setTimeout(()=>{
+            this.props.history.push('/Aboutme')
+        },1000)
     }
     gotomyskills() {
+        this.setState({out:true})
+        setTimeout(()=>{
         this.props.history.push('/Contactme')
+        },1000)
     }
     gotocontactme() {
+        this.setState({out:true})
+        setTimeout(()=>{
         this.props.history.push('/Myskills')
+        },1000)
     }
     render() {
         return (
             <div className={`${style.welcomepage}`}>
                 <div className={style.aboutcontainer}>
-                    <div className={style.aboutme}>
+                    <div className={!this.state.out? style.aboutme:style.aboutmeOUT}>
                         <div onClick={this.gotoaboutme} className={style.aboutme_image_h1_container}>
                             <div className={style.imagecontainer}>
                                 <img src={manuser} />
                             </div><div className={style.h1inmidle}>
-                                <h1 className={style.h1aboutme}>about me</h1>
+                                <h1 className={!this.state.out?style.h1aboutme:style.h1aboutmeOUT}>about me</h1>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className={style.myskillscontainer}>
-                    <div className={style.myskills}>
+                    <div className={!this.state.out?style.myskills:style.myskillsOUT}>
                         <div onClick={this.gotomyskills} className={style.aboutme_image_h1_container}>
                             <div className={style.imagecontainer}>
                                 <img src={skills} />
                             </div><div className={style.h1inmidle}>
-                                <h1 className={style.h1myskills}>my skills</h1>
+                                <h1 className={!this.state.out?style.h1myskills:style.h1myskillsOUT}>my skills</h1>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className={style.contactmecontainer}>
-                    <div className={style.contactme}>
+                    <div className={!this.state.out?style.contactme:style.contactmeOUT}>
                         <div onClick={this.gotocontactme} className={style.aboutme_image_h1_container}>
                             <div className={style.imagecontainer}>
                                 <img src={contactme} />
                             </div><div className={style.h1inmidle}>
-                                <h1 className={style.h1contactme}>contat me</h1>
+                                <h1 className={!this.state.out?style.h1contactme:style.h1contactmeOUT}>contat me</h1>
                             </div>
                         </div>
                     </div>
